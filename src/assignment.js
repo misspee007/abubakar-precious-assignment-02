dc451e4632a864d9d18fb0ca9e6d3655c1a8b27d
Precious Abubakar <71160347+misspee007@users.noreply.github.com>
2022-07-07 11:02:26 +0000

    Check challenge-1 and challenge-2. (attempted challenge 3 as well, but commented it out)

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,27 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+// function sumOfNumbers(arrayOfNumbers) {
+//   let start = 0;
+//   let end = arrayOfNumbers.length;
+//   let count = 0;
+//   do{
+//     count += arrayOfNumbers[start];
+//     start++;
+//   } while (start < end)
+  
+//   return count;
+// }
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0;
+  for(i=0; i<arrayOfNumbers.length; i++){
+    count += arrayOfNumbers[i];
+  }
+  
+  return count;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +45,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0;
+  for(i=0; i<arrayOfNumbers.length; i++){
+    if(arrayOfNumbers[i] % 2 === 0){
+      count++;
+    }
+  }
+  return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -46,9 +69,13 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
-}
+// function celsiusToFahrenheit(arrayOfNumbers) {
+//   let newArrayOfNumbers = [];
+//   for(i=0; i<arrayOfNumbers.length; i++){
+//     newArrayOfNumbers.push(Math.trunc(arrayOfNumbers[i] * 9/5 + 32));
+//   }
+//     return newArrayOfNumbers;
+// }
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
